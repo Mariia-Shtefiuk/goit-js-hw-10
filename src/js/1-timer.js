447b18a4c9562de9f2f95aec60600a5f9e2fed39
Mariia Shtefiuk <masha.shtf@gmail.com>
2025-06-14 00:32:34 +0300

    task 1 fulfill

---
+++ src/js/1-timer.js
@@ -0,0 +1,83 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const dateInput = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+startBtn.disabled = true;
+let selectedDate;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    selectedDate = selectedDates[0];
+    console.log(selectedDate);
+    if (selectedDate > new Date()) {
+      startBtn.disabled = false;
+    } else {
+      iziToast.warning({
+        title: 'Warning',
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+        timeout: 3000,
+      });
+
+      startBtn.disabled = true;
+    }
+  },
+};
+
+flatpickr(dateInput, options);
+
+startBtn.addEventListener('click', handleClick);
+
+function handleClick(event) {
+  startBtn.disabled = true;
+  dateInput.disabled = true;
+
+  const timerId = setInterval(() => {
+    const now = new Date();
+    const delta = selectedDate - now;
+
+    if (delta <= 0) {
+      clearInterval(timerId);
+      updateTimerDisplay(0, 0, 0, 0);
+      dateInput.disabled = false;
+      return;
+    }
+
+    const time = convertMs(delta);
+    updateTimerDisplay(time.days, time.hours, time.minutes, time.seconds);
+  }, 1000);
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor((ms % hour) / minute);
+  const seconds = Math.floor((ms % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function updateTimerDisplay(days, hours, minutes, seconds) {
+  document.querySelector('[data-days]').textContent = addLeadingZero(days);
+  document.querySelector('[data-hours]').textContent = addLeadingZero(hours);
+  document.querySelector('[data-minutes]').textContent =
+    addLeadingZero(minutes);
+  document.querySelector('[data-seconds]').textContent =
+    addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
