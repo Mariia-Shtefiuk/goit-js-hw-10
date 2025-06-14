f891d46ca74c4e9c347a6c3e57c80fbc11f27976
Mariia Shtefiuk <masha.shtf@gmail.com>
2025-06-14 16:07:04 +0300

    task 2 fulfill

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,41 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const delayInput = event.currentTarget.querySelector('input[name="delay"]');
+  const stateInput = event.currentTarget.querySelector(
+    'input[name="state"]:checked'
+  );
+
+  const delay = Number(delayInput.value);
+  const state = stateInput.value;
+
+  console.log(delay, state);
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(`✅ Fulfilled promise in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise in ${delay}ms`);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(msg => {
+      iziToast.success({
+        message: msg,
+        position: 'topRight',
+      });
+    })
+    .catch(msg => {
+      iziToast.error({
+        message: msg,
+        position: 'topRight',
+      });
+    });
+}
